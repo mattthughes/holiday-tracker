a3a9e361be11e6ef87d367b7e60f980d8af516bd
Matthew Hughes <matt.h17@yahoo.com>
2026-07-12 22:18:44 +0100

    Added weekdays and dates basic template

--- holiday-tracker/src/components/Calendar.jsx
+++ holiday-tracker/src/components/Calendar.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 
 const Calendar = () => {
-  return (
-    <div>
-        <h2>Calendar Component</h2>
-    </div>
-  )
-}
+    const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const availability = [
+        { date: '2024-01-01', status: "green" },
+        { date: '2024-01-02', status: "red" },
+        { date: '2024-01-03', status: "green" },
+        { date: '2024-01-04', status: "green" },
+        { date: '2024-01-05', status: "yellow" },
+    ];
+    return (
+        <div className="calendar">
+            {weekDays.map((day) => (
+                <div key={day} className="day-header">
+                    {day}
+                </div>
+            ))}
+            {availability.map((date) => {
+                return (
+                    <div key={date.date} className={`day ${date.status}`}>
+                        {date.date}
+                    </div>
+                )
+            })}
+        </div>
+        )
+        
+        
+    } 
+ 
 
-export default Calendar
+    
+  
+  
+
+
+
+
+export default Calendar;
