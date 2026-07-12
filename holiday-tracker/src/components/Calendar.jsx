b3a1d060b56e92d1884e5da36cc1918ffb89e7d1
Matthew Hughes <matt.h17@yahoo.com>
2026-07-12 23:47:51 +0100

    Added find logic to link availability with days array

--- holiday-tracker/src/components/Calendar.jsx
+++ holiday-tracker/src/components/Calendar.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 const Calendar = () => {
     const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const daysInMonth = 31;
+    const days = Array.from({ length: daysInMonth}, (_, index) => index +1)
     const availability = [
-        { date: '2024-07-1', status: "green" },
-        { date: '2024-07-02', status: "red" },
-        { date: '2024-07-03', status: "green" },
-        { date: '2024-07-04', status: "green" },
-        { date: '2024-07-05', status: "yellow" },
-        { date: '2024-07-06', status: "green" },
-        { date: '2024-07-07', status: "red" },
-        { date: '2024-07-08', status: "green" },
-        { date: '2024-07-09', status: "green" },
-        { date: '2024-07-10', status: "yellow" },
-    ];
+        {
+            day: 1,
+            status: 'available',
+            employeesOff: 3,
+            holidayLimit: 5,
+            remainingHolidays: 2    
+
+
+        }
+    ] 
+    ;
     return (
         <section className="mx-auto max-w-4xl p-6">
         <div className="grid grid-cols-7 overflow-hidden rounded-lg border text-sm font-medium text-gray-700">    
@@ -22,10 +24,11 @@ const Calendar = () => {
                     {day}
                 </div>
             ))}
-            {availability.map((date) => {
+            {days.map((day) => {
+                const availabilityInfo = availability.find(item => item.day === day)
                 return (
-                    <div key={date.date} className={`min-h-16 border p-1 text-sm text-center md:min-h-24 md:p-3 ${date.status === "green" ? "bg-green-500" : date.status === "red" ? "bg-red-500" : "bg-yellow-500"}`}>
-                        {new Date(date.date).getDate()}
+                    <div key={day} className={availabilityInfo ? "bg-green-500" : ""} min-h-16 border p-1 text-sm text-center md:min-h-24 md:p-3>
+                        {day}
                     </div>
                 )
             })}
