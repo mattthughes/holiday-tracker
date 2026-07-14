83acc4890a4a8ae0d3e34081408602730abda187
Matthew Hughes <matt.h17@yahoo.com>
2026-07-14 18:13:28 +0100

    Refactored availability logic, added availability colours

--- holiday-tracker/src/components/Calendar.jsx
+++ holiday-tracker/src/components/Calendar.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const Calendar = () => {
     const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     const daysInMonth = 31;
+    const available = 'bg-green-500';
+    const unavailable = 'bg-red-500';
+    const limited = 'bg-yellow-500';
     const days = Array.from({ length: daysInMonth}, (_, index) => index +1)
     const availability = [
         {
@@ -16,7 +19,7 @@ const Calendar = () => {
             day: 2,
             status: 'unavailable',
             employeesOff: 3,
-            holidayLimit: 5,
+            holidayLimit: 8,
             remainingHolidays: 2  
             }
             
@@ -25,6 +28,23 @@ const Calendar = () => {
 
     ] 
     ;
+    function getAvailabilityStatus(day) {
+        console.log(day)
+        const availabilityInfo = availability.find(item => item.day === day)
+        console.log(availabilityInfo)
+        if (!availabilityInfo) {
+            return "bg-blue-500";
+        }
+        const holidayStatus = availabilityInfo.status;
+        
+        if (holidayStatus === 'unavailable') {
+             return unavailable;
+        } else if (holidayStatus === 'limited') {
+             return limited;
+        } else {
+             return available;
+        }
+    }
     return (
         <section className="mx-auto max-w-4xl p-6">
         <div className="grid grid-cols-7 overflow-hidden rounded-lg border text-sm font-medium text-gray-700">    
@@ -34,17 +54,15 @@ const Calendar = () => {
                 </div>
             ))}
             {days.map((day) => {
-                const availabilityInfo = availability.find(item => item.day === day)
                 return (
-                    <div key={day} className={availabilityInfo ? availabilityInfo.status === 'available' ? 'bg-green-500' :  'bg-red-500' : ''} min-h-16 border p-1 text-sm text-center md:min-h-24 md:p-3>
+<div key={day} className={`day ${getAvailabilityStatus(day)}`} min-h-16 border p-1 text-sm text-center md:min-h-24 md:p-3>
                         {day}
-                        {availabilityInfo && (
-                            <div className="mt-2 text-xs text-gray-500">
-                                <p>{availabilityInfo.status}</p>
-                            </div>
-                        )}
+                        
                     </div>
                 )
+
+                    
+                
             })}
         </div>
         </section>
